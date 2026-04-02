7e7ddeb246153b3cce5c766bc3240bc5c25a7647
PunchMonkey84 <sathasivamth@wgc.school.nz>
2026-04-02 14:30:12 +1300

    Add files via upload

--- gamble.py
+++ gamble.py
@@ -7,35 +7,51 @@
 import random
 
 # Symbols
-row = ["\U0001F911", "💎", "💰"]
-column = ["\U0001F911", "💎", "💰"]
+row = ["🧠", "💎", "💰"]
+column = ["🧠", "💎", "💰"]
 
 money = int(input("How much money are you ready to risk: "))
 winnings = 0
 
-while money >= 0:
-    money -= 10
-# ask user how much lines to check
-    lines = int(input("what line do you want to check(1,2,3): "))
+
+
+# Functions
+
+# Making of the Grid
+def generate_grid(row, column):
 
 # Create a loop that prints random places for the symbols
     for i in range(len(row)):
-    
-    # Create a list to store the random rows to check for win
-        wose = []
+
+    # Create a list to store the random rows to check for a win
+        grid = []
 
     # printing slots
         for j in range(len(column)):
             random.shuffle(row)
-            wose.append(row[0])
-            print((row[0]), end ="   ")
-        print()
 
-# checking according to the amount of lines they want to check
-    for k in range(lines):
+        grid.append(row)
+        return grid
+
+while money >= 0:
+    money -= 10
+
+    # Ask user how many lines to check
+    lines = int(input("what line do you want to check(1,2,3): "))
+
+    # generating slots
+    slots = generate_grid(row, column)
+    print(slots)
+
+    for r in slots:
+        print(" | ".join(r))
+    print()
+        
+# checking according to the number of lines they want to check
+    for i in range(lines):
 
     # checking lines
-        if wose[0] == wose[1] == wose[2]:
+        if slots[i][0] == slots[i][1] == slots[i][2]:
             win = True
             winnings += 100
             
@@ -50,5 +66,3 @@ while money >= 0:
 
 # display how much they have left
 print("You have won ${}".format(winnings))
-
-
